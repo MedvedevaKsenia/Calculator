2bc32631fd311169bcaec92308800f1edeeb7468
Ksenia <ksenia.medvedeva.1993@gmail.com>
2024-02-18 19:24:36 +0500

    fix after comments

--- src/main/java/ru/kseniamedvedeva/calcapp/_main/_Main.java
+++ src/main/java/ru/kseniamedvedeva/calcapp/_main/_Main.java
@@ -5,10 +5,8 @@ import ru.kseniamedvedeva.calcapp.util.Calculator;
 
 public class _Main {
     public static void main(String[] args) {
-
        int a = 333;
        int b = 33;
-
        ResultWriterService.printResult(a,b,Calculator.add(a,b),"plus");
        ResultWriterService.printResult(a,b,Calculator.subtract(a,b),"minus");
        ResultWriterService.printResult(a,b,Calculator.multiply(a,b),"x");
--- src/main/java/ru/kseniamedvedeva/calcapp/util/Calculator.java
+++ src/main/java/ru/kseniamedvedeva/calcapp/util/Calculator.java
@@ -3,17 +3,14 @@ package ru.kseniamedvedeva.calcapp.util;
 public class Calculator {
 
     public static int add(int num1, int num2) {
-        int sum = num1 + num2;
-        return sum;
+        return num1 + num2;
     }
 
     public static int subtract(int num1, int num2) {
-        int difference = num1 - num2;
-        return difference;
+        return num1 - num2;
     }
 
     public static int multiply(int num1, int num2) {
-        int product = num1 * num2;
-        return product;
+        return num1 * num2;
     }
 }
