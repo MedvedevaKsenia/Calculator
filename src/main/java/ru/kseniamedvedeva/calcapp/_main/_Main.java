1d8d21b0bc04c288de377a0962e5505ce711baeb
Ksenia <ksenia.medvedeva.1993@gmail.com>
2024-02-20 21:35:48 +0500

    fix after comments 2

--- src/main/java/ru/kseniamedvedeva/calcapp/_main/_Main.java
+++ src/main/java/ru/kseniamedvedeva/calcapp/_main/_Main.java
@@ -5,11 +5,11 @@ import ru.kseniamedvedeva.calcapp.util.Calculator;
 
 public class _Main {
     public static void main(String[] args) {
-       int a = 333;
-       int b = 33;
-       ResultWriterService.printResult(a,b,Calculator.add(a,b),"plus");
-       ResultWriterService.printResult(a,b,Calculator.subtract(a,b),"minus");
-       ResultWriterService.printResult(a,b,Calculator.multiply(a,b),"x");
+        int a = 333;
+        int b = 33;
+        ResultWriterService.printResult(a, b, Calculator.add(a, b), "plus");
+        ResultWriterService.printResult(a, b, Calculator.subtract(a, b), "minus");
+        ResultWriterService.printResult(a, b, Calculator.multiply(a, b), "x");
     }
 
 }
